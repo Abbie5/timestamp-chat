0bf0f98e9fef08f58a6bcd31d45b91209179f423
Abigail Ient <abigail@ient.me>
2022-06-25 13:17:07 +0100

    Remove qsl dependency

--- src/main/java/cc/abbie/timestamp_chat/PreLaunch.java
+++ src/main/java/cc/abbie/timestamp_chat/PreLaunch.java
@@ -3,11 +3,11 @@ package cc.abbie.timestamp_chat;
 import me.shedaniel.autoconfig.AutoConfig;
 import me.shedaniel.autoconfig.serializer.Toml4jConfigSerializer;
 import org.quiltmc.loader.api.ModContainer;
-import org.quiltmc.qsl.base.api.entrypoint.client.ClientModInitializer;
+import org.quiltmc.loader.api.entrypoint.PreLaunchEntrypoint;
 
-public class ClientMod implements ClientModInitializer {
+public class PreLaunch implements PreLaunchEntrypoint {
     @Override
-    public void onInitializeClient(ModContainer mod) {
+    public void onPreLaunch(ModContainer mod) {
         AutoConfig.register(Config.class, Toml4jConfigSerializer::new);
     }
 }
