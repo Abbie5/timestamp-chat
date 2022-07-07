f41fe6bfc94b9e85cc0639bd7c63a5a7162c3d98
Abigail Ient <abigail@ient.me>
2022-07-07 23:02:53 +0100

    Add option to change timestamp colour
    Bump version

--- src/main/java/cc/abbie/timestamp_chat/config/Config.java
+++ src/main/java/cc/abbie/timestamp_chat/config/Config.java
@@ -1,8 +1,12 @@
 package cc.abbie.timestamp_chat.config;
 
 import me.shedaniel.autoconfig.ConfigData;
+import me.shedaniel.autoconfig.annotation.ConfigEntry;
 
 @me.shedaniel.autoconfig.annotation.Config(name = "timestamp_chat")
 public class Config implements ConfigData {
 	public String pattern = "HH:mm";
+
+	@ConfigEntry.ColorPicker
+	public int color = 0x888888;
 }
--- src/main/java/cc/abbie/timestamp_chat/mixin/ChatMixin.java
+++ src/main/java/cc/abbie/timestamp_chat/mixin/ChatMixin.java
@@ -3,9 +3,8 @@ package cc.abbie.timestamp_chat.mixin;
 import cc.abbie.timestamp_chat.config.Config;
 import me.shedaniel.autoconfig.AutoConfig;
 import net.minecraft.client.gui.hud.ChatHud;
-import net.minecraft.text.MutableText;
+import net.minecraft.text.Style;
 import net.minecraft.text.Text;
-import net.minecraft.text.TextContent;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.ModifyVariable;
@@ -16,13 +15,16 @@ import java.util.Date;
 
 @Mixin(ChatHud.class)
 public class ChatMixin {
-	@ModifyVariable(method = "addMessage(Lnet/minecraft/text/Text;)V", at = @At("HEAD"), argsOnly = true)
-	public Text onAddMessage(Text message) {
-		Config config = AutoConfig.getConfigHolder(Config.class).getConfig();
+    @ModifyVariable(method = "addMessage(Lnet/minecraft/text/Text;)V", at = @At("HEAD"), argsOnly = true)
+    public Text onAddMessage(Text message) {
+        Config config = AutoConfig.getConfigHolder(Config.class).getConfig();
 
-		return MutableText.of(TextContent.EMPTY)
-				.append(new SimpleDateFormat(config.pattern).format(new Date()))
-				.append(" ")
-				.append(message);
-	}
+        return Text.empty()
+                .append(
+                        Text.literal(new SimpleDateFormat(config.pattern).format(new Date()))
+                                .setStyle(Style.EMPTY.withColor(config.color))
+                )
+                .append(" ")
+                .append(message);
+    }
 }
