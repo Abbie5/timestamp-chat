a73141f35d4d82263dc6c1a39b627a65e6d1d9fe
Abbie <abigail@ient.me>
2022-08-02 18:54:12 +0100

    Update to 1.19.1

--- src/main/java/cc/abbie/timestamp_chat/mixin/ChatMixin.java
+++ src/main/java/cc/abbie/timestamp_chat/mixin/ChatMixin.java
@@ -9,7 +9,7 @@ import org.spongepowered.asm.mixin.injection.ModifyVariable;
 
 @Mixin(ChatComponent.class)
 public class ChatMixin {
-    @ModifyVariable(method = "addMessage(Lnet/minecraft/network/chat/Component;)V", at = @At("HEAD"), argsOnly = true)
+    @ModifyVariable(method = "addMessage(Lnet/minecraft/network/chat/Component;Lnet/minecraft/network/chat/MessageSignature;Lnet/minecraft/client/GuiMessageTag;)V", at = @At("HEAD"), argsOnly = true)
     private Component onAddMessage(Component message) {
         return Component.empty().append(Timestamp.create()).append(" ").append(message);
     }
